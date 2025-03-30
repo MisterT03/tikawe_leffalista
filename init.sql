6a0cf53b7e74042d32c122a5116d0f63c9699899
MisterT03 <159497247+MisterT03@users.noreply.github.com>
2025-03-30 18:02:53 +0300

    init.sql

--- init.sql
+++ init.sql
@@ -1,1 +1,17 @@
-#.
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS movies;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE movies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    genre TEXT NOT NULL,
+    rating INTEGER,
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
