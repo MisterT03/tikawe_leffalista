cff28b979d67c8f29743d77a839e0764090a16b8
MisterT03 <159497247+MisterT03@users.noreply.github.com>
2025-03-30 18:02:19 +0300

    db.py

--- db.py
+++ db.py
@@ -1,1 +1,6 @@
-#db
+import sqlite3
+
+def get_db_connection():
+    conn = sqlite3.connect("database.db")  # Luo tai avaa tietokanta
+    conn.row_factory = sqlite3.Row  # Tulokset sanakirjamaisina objekteina
+    return conn
