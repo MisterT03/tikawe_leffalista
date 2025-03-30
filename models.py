ba020265b88e50c3686e4037cef78c106f0c97f6
MisterT03 <159497247+MisterT03@users.noreply.github.com>
2025-03-30 18:01:35 +0300

    models.py

---
+++ models.py
@@ -0,0 +1,22 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    genre = db.Column(db.String(50), nullable=False)
+    rating = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('movies', lazy=True))
+
+    def __repr__(self):
+        return f'<Movie {self.title}>'
